75695d97f74f37ce7c996bc43e47e49e011942eb
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-05-20 00:34:26 -0400

    Add code to update post count on post changes

--- extend.php
+++ extend.php
@@ -13,10 +13,31 @@ namespace Askvortsov\FlarumCategories;
 
 use Askvortsov\FlarumCategories\Console\RecalculateTagStats;
 use Flarum\Extend;
+use Flarum\Post\Event\Hidden;
+use Flarum\Post\Event\Posted;
+use Flarum\Post\Event\Restored;
 use FoF\Components\Extend\AddFofComponents;
 use Illuminate\Contracts\Events\Dispatcher;
 
+/**
+ * @param \Flarum\Post\Post $post
+ * @param int $delta
+ */
+function updateTagsPostCount($post, $delta)
+{
+    if (!$post) {
+        return;
+    }
 
+    foreach ($post->discussion->tags as $tag) {
+        // We do not count private discussions in tags
+        if (!$post->is_private && !$post->discussion->is_private) {
+            $tag->post_count += $delta;
+        }
+
+        $tag->save();
+    }
+}
 
 return [
     new AddFofComponents(),
@@ -34,4 +55,15 @@ return [
     new Extend\Locales(__DIR__ . '/resources/locale'),
 
     (new Extend\Console)->command(RecalculateTagStats::class),
+
+    (new Extend\Event)
+        ->listen(Hidden::class, function (Hidden $event) {
+            updateTagsPostCount($event->post, -1);
+        })
+        ->listen(Posted::class, function (Posted $event) {
+            updateTagsPostCount($event->post, 1);
+        })
+        ->listen(Restored::class, function (Restored $event) {
+            updateTagsPostCount($event->post, 1);
+        }),
 ];

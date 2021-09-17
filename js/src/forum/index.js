a1fd9c836f7e2895041552c05d96cd2a946139d1
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-09-17 13:39:14 -0400

    Don't show tags on top of page when in category page

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -5,6 +5,16 @@ import Model from 'flarum/common/Model';
 import Tag from 'flarum/tags/models/Tag';
 import CategoriesPage from './components/CategoriesPage';
 
+function pruneIndexNav(items, func) {
+  if (app.current.matches(CategoriesPage) && app.forum.attribute('categories.fullPageDesktop')) {
+    for (const item in items.items) {
+      if (func(item)) {
+        items.remove(item);
+      }
+    }
+  }
+}
+
 app.initializers.add('askvortsov/flarum-categories', () => {
   app.routes.categories = {
     path: '/categories',
@@ -29,17 +39,13 @@ app.initializers.add('askvortsov/flarum-categories', () => {
       items.replace('moreTags', <LinkButton href={app.route('categories')}>{app.translator.trans('flarum-tags.forum.index.more_link')}</LinkButton>);
     }
 
+    pruneIndexNav(items, (item) => item.startsWith('tag'));
+
     return items;
   });
 
   extend(IndexPage.prototype, 'sidebarItems', function (items) {
-    if (app.current.matches(CategoriesPage) && app.forum.attribute('categories.fullPageDesktop')) {
-      for (const item in items.items) {
-        if (item !== 'newDiscussion' && item !== 'nav') {
-          items.remove(item);
-        }
-      }
-    }
+    pruneIndexNav(items, (item) => item !== 'newDiscussion' && item !== 'nav');
     return items;
   });
 });
